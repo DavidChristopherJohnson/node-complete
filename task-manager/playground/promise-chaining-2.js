167af5fb260a25d4ffe668ea7bc135575c76c6a0
David Johnson <davidjohnson@davids-mac-book.local>
2020-09-25 15:24:20 +0100

    Delete async excersises added

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,14 +1,25 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-const _id = '5f6c70ce27176806cfe979a1';
+const _id = '5f2be58900912b0e04b4114d';
 
-Task.findByIdAndDelete(_id)
-    .then((task) => {
-        console.log(task);
-        return Task.countDocuments({ completed: false });
-    }).then((value) => {
-        console.log(`Count: ${value}`);
-    }).catch(e => {
-        console.log(e);
-    });
+// Task.findByIdAndDelete(_id)
+//     .then((task) => {
+//         console.log(task);
+//         return Task.countDocuments({ completed: false });
+//     }).then((value) => {
+//         console.log(`Count: ${value}`);
+//     }).catch(e => {
+//         console.log(e);
+//     });
+
+const deleteAndCount = async (id) => {
+    const deleted = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false });
+
+    return count;
+};
+
+deleteAndCount(_id)
+    .then(count => console.log("Incomplete: ", count))
+    .catch(e => console.log("Error: ", e));
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -3,14 +3,25 @@ const User = require('../src/models/user');
 
 const _id = '5f6c693aea2100062736bcc6';
 
-User.findByIdAndUpdate(_id, {
-    age: 7
-}).then((user) => {
-    console.log(user);
+// User.findByIdAndUpdate(_id, {
+//     age: 7
+// }).then((user) => {
+//     console.log(user);
 
-    return User.countDocuments({ age: 7 });
-}).then((value) => {
-    console.log(`Count: ${value}`);
-}).catch(e => {
-    console.log(e);
-});
+//     return User.countDocuments({ age: 7 });
+// }).then((value) => {
+//     console.log(`Count: ${value}`);
+// }).catch(e => {
+//     console.log(e);
+// });
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const countValues = await User.countDocuments({ age });
+
+    return countValues;
+}
+
+updateAgeAndCount(_id, 7)
+    .then(count => console.log("Count: ", count))
+    .catch(e => console.log("Error: ", e));
